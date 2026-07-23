bb272bc7e9f42c7447cbd83b574cbda69f242a0e
vivek kumar <vivekkumar236780@gmail.com>
2026-07-23 15:53:29 +0530

    initial commit

---
+++ DAY1.js
@@ -0,0 +1,44 @@
+console.log("hello world");
+//variables
+ 
+//var
+var name="vivek";
+console.log(name);
+console.log(typeof(name));
+
+var name="aman"; //redeclare
+console.log(name);
+
+//let
+let age=22;
+console.log(age); //can not be redeclare
+
+//constant
+const c="india";
+console.log(c);
+
+//datatypes
+let a=1;
+let b="vishal";
+let m=true;
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(m));
+
+let s={
+    name:"aman"
+
+}
+console.log(s);
+
+//operator
+let p=22;
+let q=15;
+console.log(p+q);
+console.log(p-q);
+console.log(p*q);
+console.log(p/q);
+console.log(p%q);
+console.log(++p);
+
+
