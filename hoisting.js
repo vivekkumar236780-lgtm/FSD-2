f82f7750a104ffc1b75458f0ec4f814cc84aad88
vivek kumar <vivekkumar236780@gmail.com>
2026-09-24 10:27:45 +0530

    initial commit

--- hoisting.js
+++ hoisting.js
@@ -7,10 +7,9 @@ function outer(){
     let counter=0;
     function inner(){
         counter++;
-
         console.log(counter);
     }
-    return inner();
+    return inner;
 }
 let i=outer();
 i();
@@ -32,4 +31,41 @@ function mul(p,q){
 console.log(calculate(10,20,add));
 console.log(calculate(4,6,mul));
 
+//method calls
+const person={
+    name:"Vivek",
+    age:21,
+    greet:function(){
+        return this.name;
+    }
+};
+let p1=person.greet.bind(person);
+console.log(p1());
+
+const Person={
+    name:"Amit",
+    greet(){
+        console.log(this.name);
+    }
+};
+setTimeout(Person.greet.bind(Person), 1000);
+
+function multiply(a,b){
+    return a*b;
+
+}
+const double=multiply.bind(null,2);
+console.log(double(5));
+
+//constructor call
+function PERSON(name){
+    this.name=name;
+
+}
+PERSON.prototype.getName=function(){
+    return this.name;
+}
+let P1=new PERSON("Aman");
+console.log(P1.name);
+console.log(P1.getName());
 
--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 
 let num=[16,8,9,2,7];
-for(let i=0;i<num.size();i++){
+for(let i=0;i<num.length;i++){
     num[i]=num[i]*num[i];
     console.log(num[i]);
 }
@@ -12,7 +12,7 @@ let square =numbers.map(function(number){
 console.log(square);
 //using arrow function
 let nums=[1,2,3,4,5];
-let sqr =numbers.map(number=>number*number);
+let sqr =nums.map(number=>number*number);
 console.log(sqr);
 
 //this keyword
@@ -23,7 +23,36 @@ const person={
         console.log(this);
     }
 };
+console.log(this);
 person.greet();
 person.name="Yuvaan";
 person.age=5;
 person.greet();
+
+function one(){
+    let user="Arvind";
+    console.log(this.user);
+}
+one(); 
+
+console.log(this === globalThis);
+this.color='Blue';
+console.log(global.color);
+
+//non strict mode
+function show(){
+    console.log(this===global);
+
+}
+show();
+
+function outer(){
+    "use strict";
+    console.log(this === undefined);
+    function inner(){
+        console.log(this === undefined);
+
+    }
+    inner();
+}
+outer();
